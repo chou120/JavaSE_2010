1e674454d48b86aca425147b54b2a16ef23b7021
周健 <zhoujian@banyuan.club>
2020-11-09 14:41:00 +0800

    远程关联git

---
+++ JavaBasic/src/club/banyuan/core/Main.java
@@ -0,0 +1,14 @@
+package club.banyuan.core;
+
+/**
+ * @author sanye
+ * @version 1.0
+ * @date 2020/11/9 2:38 下午
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("我是你爸爸....");
+  }
+
+}
